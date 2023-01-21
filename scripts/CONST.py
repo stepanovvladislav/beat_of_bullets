a2b4464f29049de96e4b5e61b588cf9247530c6b
0veR6amE <oleg.manannikov@yandex.ru>
2023-01-21 19:30:13 +0300

    add PygameSprite

--- scripts/CONST.py
+++ scripts/CONST.py
@@ -1,9 +1,14 @@
 import os
 
+from scripts.main.data import *
+
 Running = False
 
 Name = "BoB"
 Version = "16012023"
+currentSkin = "user"
+windowManager = WindowManager()
+cursorPos = vector2(0, 0)
 currentDirectory = os.getcwd()
 surface = None
 Config = None
